938b4bc44803c491c1a4c6c3ff74da7e99561301
Serhii Stepanov <serhii.stepanov.flutter.dev@gmail.com>
2025-05-03 13:24:04 +0200

    feat: l10n integration

--- lib/app/core/extensions/build_context_ext.dart
+++ lib/app/core/extensions/build_context_ext.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:vocabulary/app/l10n/generated/app_localizations.dart';
 
 extension BuildContextExt on BuildContext {
   GoRouter get router => GoRouter.of(this);
   ThemeData get theme => Theme.of(this);
+  AppLocalizations get localizations => AppLocalizations.of(this)!;
 }
