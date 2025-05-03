04e70b156d4f2be9c1d32912432311ed92b463af
Serhii Stepanov <serhii.stepanov.flutter.dev@gmail.com>
2025-05-03 12:28:01 +0200

    feat: router integration

---
+++ lib/app/router/router.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
+import 'package:vocabulary/app/router/config/screen.dart';
+import 'package:vocabulary/presentation/splash/splash_screen.dart';
+
+abstract class AppRouterProvider {
+  static final _rootNavigatorKey = GlobalKey<NavigatorState>();
+  static final _router = _buildGoRouter(rootNavigatorKey: _rootNavigatorKey);
+
+  static GoRouter get router => _router;
+}
+
+GoRouter _buildGoRouter({required GlobalKey<NavigatorState> rootNavigatorKey}) {
+  return GoRouter(
+    navigatorKey: rootNavigatorKey,
+    initialLocation: '/${Screen.splash}',
+    routes: [
+      GoRoute(
+        path: '/${Screen.splash}',
+        name: Screen.splash,
+        builder: (context, state) => const SplashScreen(),
+      ),
+    ],
+  );
+}
