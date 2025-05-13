209571a8dc24731e6a4f89094a1e5d748215c9bf
Serhii Stepanov <serhii.stepanov.flutter.dev@gmail.com>
2025-05-13 20:52:23 +0200

    feat: swipe words with tap.

--- lib/presentation/core/carousel_slider_widget.dart
+++ lib/presentation/core/carousel_slider_widget.dart
@@ -1,32 +1,29 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
-class CarouselSliderWidget extends StatefulWidget {
+class CarouselSliderWidget extends StatelessWidget {
+  final CarouselSliderController? carouselController;
   final int itemCount;
   final ExtendedIndexedWidgetBuilder itemBuilder;
   const CarouselSliderWidget({
     super.key,
+    this.carouselController,
     required this.itemCount,
     required this.itemBuilder,
   });
 
   @override
-  State<CarouselSliderWidget> createState() => CarouselSliderWidgetState();
-}
-
-class CarouselSliderWidgetState extends State<CarouselSliderWidget> {
-
-  @override
   Widget build(BuildContext context) {
     final height = MediaQuery.sizeOf(context).height;
     return CarouselSlider.builder(
+      carouselController: carouselController,
       options: CarouselOptions(
         scrollDirection: Axis.vertical,
         height: height,
         viewportFraction: 1.0,
       ),
-      itemCount: widget.itemCount,
-      itemBuilder: widget.itemBuilder,
+      itemCount: itemCount,
+      itemBuilder: itemBuilder,
     );
   }
 }
