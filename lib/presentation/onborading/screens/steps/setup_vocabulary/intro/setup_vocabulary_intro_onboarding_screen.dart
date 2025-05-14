79fbe451bf07a5a49475d84de7b1c28f5e4d241c
Serhii Stepanov <serhii.stepanov.flutter.dev@gmail.com>
2025-05-14 02:53:26 +0200

    refactor: use default onboarding intro scaffold.

--- lib/presentation/onborading/screens/steps/setup_vocabulary/intro/setup_vocabulary_intro_onboarding_screen.dart
+++ lib/presentation/onborading/screens/steps/setup_vocabulary/intro/setup_vocabulary_intro_onboarding_screen.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:vocabulary/app/core/extensions/build_context_ext.dart';
 import 'package:vocabulary/presentation/onborading/bloc/onboarding_bloc.dart';
-import 'package:vocabulary/presentation/onborading/config/onboarding_pop_screen_handler.dart';
 import 'package:vocabulary/presentation/onborading/data/onboarding_step_ui.dart';
-import 'package:vocabulary/presentation/onborading/widgets/scaffold/onboarding_intro_scaffold.dart';
+import 'package:vocabulary/presentation/onborading/widgets/scaffold/default/onboarding_default_intro_scaffold.dart';
 
 class SetupVocabularyIntroOnboardingScreen extends StatelessWidget {
   const SetupVocabularyIntroOnboardingScreen({super.key});
@@ -14,18 +13,9 @@ class SetupVocabularyIntroOnboardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final bloc = context.read<OnboardingBloc>();
-    return OnboardingIntroScaffold(
-      onPop: () {
-        OnboardingPopScreenHandler.onPop(context, bloc);
-      },
-      onContinue: () {
-        bloc.add(const OnboardingEvent.markStepAsCompleted(step));
-        bloc.add(const OnboardingEvent.goToNextStep());
-      },
-      onSkip: () {
-        bloc.add(const OnboardingEvent.skip());
-      },
-      isLastOnboardingStep: bloc.state.isLastOnboardingStep(step),
+    return OnboardingDefaultIntroScaffold(
+      bloc: bloc,
+      step: step,
       title: context.localizations.setUpVocabularyToHelpYouAchieveYourGoals,
     );
   }
